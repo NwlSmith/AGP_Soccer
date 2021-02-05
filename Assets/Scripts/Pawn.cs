56fe82ec43b6b45fe5173a1ef91105b55adcc259
Nate Smith <me@nwlsmith.com>
2021-02-05 17:59:17 -0500

    Forgot to fully commit all items in last commit oops

--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -6,22 +6,33 @@ public class Pawn : MonoBehaviour
 {
 
     public bool isPlayer = false;
-    public float speed = 5f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float kickForceHorizontal = 10f;
+    [SerializeField] private float kickForceVertical = 2f;
 
-    // Start is called before the first frame update
-    void Start()
+    public void AIMove(Vector3 moveDirection)
     {
-        
+        Vector3 movementVector = moveDirection * speed * Time.deltaTime;
+        // Actually move
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (!collision.gameObject.CompareTag("Ball"))
+            return;
+
+        Kick();
     }
 
-    public void Move(Vector3 moveDirection)
+    private void Kick()
     {
+        // Similar issue with CalculateAIMovement() in AILifetimeController, I don't think this calculates what I want it to.
+        Vector3 forceDirection = ServicesLocator.ball.position - transform.position;
+        forceDirection.y = 0f;
+        forceDirection.Normalize();
+        forceDirection *= kickForceHorizontal;
+        forceDirection.y = kickForceVertical;
 
+        ServicesLocator.ball.AddForce(forceDirection, ForceMode.Impulse);
     }
 }
